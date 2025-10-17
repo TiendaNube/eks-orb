7c60d83f6519cc6a773fb75e80c2166fbf9947f7
Juan G. Arias <juan.arias@tiendanube.com>
2025-10-17 12:52:05 -0300

    Output Helm manifest details
    Enhance Helm version detection script to include manifest details for both Helm v2 and v3 releases. Added output of the release manifest to improve visibility into deployment details.

--- src/scripts/detect_helm_version.sh
+++ src/scripts/detect_helm_version.sh
@@ -53,6 +53,9 @@ function detect_helmv3() {
       chart_name=$(yq eval 'map(select(.status == "deployed")) | .[0].chart // ""' "${DETECTION_DIR}/helm_v3_history.yaml" | sed -E 's/-[0-9]+\.[0-9]+\.[0-9]+$//')
     fi
     echo "✅ Helm v3 release detected"
+    echo "- Manifest details 📄 --------------------------------------------------"
+    helmv3 get manifest "${release_name}" --namespace "${namespace}"
+    echo "------------------------------------------------------------------------"
     write_result "helmv3" "${chart_name:-}"
     return 0
   elif echo "${HELM_V3_OUTPUT}" | grep -q 'release: not found'; then
@@ -83,6 +86,9 @@ function detect_helmv2() {
       chart_name=$(yq eval 'map(select(.status == "DEPLOYED")) | .[0].chart // ""' "${DETECTION_DIR}/helm_v2_history.yaml" | sed -E 's/-[0-9]+\.[0-9]+\.[0-9]+$//')
     fi
     echo "✅ Helm v2 release detected"
+    echo "- Manifest details 📄 --------------------------------------------------"
+    helm get manifest "${release_name}"
+    echo "------------------------------------------------------------------------"
     write_result "helmv2" "${chart_name:-}"
     return 0
   elif echo "${HELM_V2_OUTPUT}" | grep -q 'not found' || echo "${HELM_V2_OUTPUT}" | grep -q 'release: ".*" not found'; then
